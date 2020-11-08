a1202bafe890c85343cc52660c2c2a686aaa13a3
labyrinth-brno <naprogramujhru@gmail.com>
2020-11-08 11:03:27 +0100

    přidáno kb

--- Python-CSV/parse_csv_kb.py
+++ Python-CSV/parse_csv_kb.py
@@ -1,14 +1,15 @@
 import csv
 
-with open('names.csv', 'r') as csv_file:
+with open('kb11.csv', 'r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
 
-    with open('new_names.csv', 'w') as new_file:
-        fieldnames = ['first_name', 'last_name']
+    with open('new_kb11.csv', 'w') as new_file:
+        fieldnames = ['Datum splatnosti', 'castka']
 
         csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter='\t')
 
         csv_writer.writeheader()
 
         for line in csv_reader:
+            del line['email']
             csv_writer.writerow(line)
